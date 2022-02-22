404fae3a086c2e65513bff0ca5431f0438baa946
小冰鸡 <9266747+iced-chicken@user.noreply.gitee.com>
2022-02-22 19:18:11 +0800

    前端项目搭建环境

---
+++ granduation-design/src/main.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+import '@/assets/style/base.css'
+
+// import '@/config/ele.config.js'
+import ElementUI from "element-ui";
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
