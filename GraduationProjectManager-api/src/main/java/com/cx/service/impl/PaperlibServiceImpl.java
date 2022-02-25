f62940eab94e4b16fc1571a93c3ddf556df6cd03
Karma <409433628@qq.com>
2022-02-25 19:44:50 +0800

    update

--- GraduationProjectManager-api/src/main/java/com/cx/service/impl/PaperlibServiceImpl.java
+++ GraduationProjectManager-api/src/main/java/com/cx/service/impl/PaperlibServiceImpl.java
@@ -4,10 +4,8 @@ import cn.org.atool.fluent.mybatis.model.StdPagedList;
 import com.cx.fluentmybatis.entity.PaperlibEntity;
 import com.cx.fluentmybatis.mapper.PaperlibMapper;
 import com.cx.fluentmybatis.wrapper.PaperlibQuery;
-import com.cx.fluentmybatis.wrapper.StudentQuery;
 import com.cx.model.PageReq;
 import com.cx.service.PaperlibService;
-import com.sun.org.glassfish.gmbal.ManagedAttribute;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
