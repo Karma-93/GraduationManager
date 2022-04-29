c55a98168a52b7ff0e78d1d74dcd93a99ad5ae91
小冰鸡 <9266747+iced-chicken@user.noreply.gitee.com>
2022-04-29 13:37:39 +0800

    选题管理界面美化

--- GraduationProjectManager-front/src/views/admin/AProject.vue
+++ GraduationProjectManager-front/src/views/admin/AProject.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <!-- <a-row>
+        <a-card>
+            <!-- <a-row>
             <a-space>
                 <a-radio-group v-model="status">
                     <a-radio-button value="all">全部</a-radio-button>
@@ -18,65 +19,69 @@
                 </a-button>
             </a-space>
         </a-row> -->
-        <a-button type="primary" style="margin:10px 0" @click="clickEdit(null)"
-            >添加</a-button
-        >
-        <a-table :data-source="data">
-            <a-table-column
-                key="projectDescribe"
-                title="课题表述"
-                data-index="projectDescribe"
-                align="center"
-            />
-            <a-table-column
-                key="projectFromId"
-                title="课题来源"
-                data-index="projectFromId"
-                align="center"
-            />
-            <a-table-column
-                key="projectName"
-                title="课题名"
-                data-index="projectName"
-                align="center"
-            />
-
-            <!-- 每一列的显示控制 -->
-            <a-table-column
-                key="projectStatus"
-                title="选题状态"
-                data-index="projectStatus"
-                align="center"
+            <a-button
+                type="primary"
+                style="margin:10px 0"
+                @click="clickEdit(null)"
+                >添加</a-button
             >
-                <!-- 列里边的模板，text代表该行的列数据,record代表该行整体对象数据，这里record.projectStatus就等于text -->
-                <template slot-scope="text">
-                    <a-tag v-if="text == 0" color="blue">
-                        未选择
-                    </a-tag>
-                    <a-tag v-if="text == 1" color="green">已被选</a-tag>
-                </template>
-            </a-table-column>
-            <a-table-column
-                key="studentId"
-                title="选题学生"
-                data-index="studentId"
-                align="center"
-            />
-            <!-- 不写template模板的话，直接显示该行数据对应的key配对的值 -->
-            <a-table-column
-                key="teacherId"
-                title="指导老师"
-                data-index="teacherId"
-                align="center"
-            />
-            <a-table-column key="action" title="操作" align="center">
-                <template slot-scope="text, record">
-                    <a-button type="link" @click="clickEdit(record)"
-                        >编辑</a-button
-                    >
-                </template>
-            </a-table-column>
-        </a-table>
+            <a-table :data-source="data">
+                <a-table-column
+                    key="projectDescribe"
+                    title="课题表述"
+                    data-index="projectDescribe"
+                    align="center"
+                />
+                <a-table-column
+                    key="projectFromId"
+                    title="课题来源"
+                    data-index="projectFromId"
+                    align="center"
+                />
+                <a-table-column
+                    key="projectName"
+                    title="课题名"
+                    data-index="projectName"
+                    align="center"
+                />
+
+                <!-- 每一列的显示控制 -->
+                <a-table-column
+                    key="projectStatus"
+                    title="选题状态"
+                    data-index="projectStatus"
+                    align="center"
+                >
+                    <!-- 列里边的模板，text代表该行的列数据,record代表该行整体对象数据，这里record.projectStatus就等于text -->
+                    <template slot-scope="text">
+                        <a-tag v-if="text == 0" color="blue">
+                            未选择
+                        </a-tag>
+                        <a-tag v-if="text == 1" color="red">已被选</a-tag>
+                    </template>
+                </a-table-column>
+                <a-table-column
+                    key="studentId"
+                    title="选题学生"
+                    data-index="studentId"
+                    align="center"
+                />
+                <!-- 不写template模板的话，直接显示该行数据对应的key配对的值 -->
+                <a-table-column
+                    key="teacherId"
+                    title="指导老师"
+                    data-index="teacherId"
+                    align="center"
+                />
+                <a-table-column key="action" title="操作" align="center">
+                    <template slot-scope="text, record">
+                        <a-button type="link" @click="clickEdit(record)"
+                            >编辑</a-button
+                        >
+                    </template>
+                </a-table-column>
+            </a-table>
+        </a-card>
     </div>
 </template>
 
